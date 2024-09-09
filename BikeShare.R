cd7b5330d40f0af4c07268deaf0311cc5e58d3b5
Josh Bergstrom <jbergs12@byu.edu>
2024-09-09 09:52:05 -0600

    Some EDA

--- BikeShare.R
+++ BikeShare.R
@@ -1,2 +1,26 @@
 library(tidyverse)
 library(tidymodels)
+library(vroom)
+library(patchwork)
+library(skimr)
+library(DataExplorer)
+library(GGally)
+
+bike_train <- vroom("train.csv")
+View(bike_train)
+
+glimpse(bike_train)
+skimr(bike_train)
+
+bike_train |> 
+  ggplot(aes(x=temp,
+             y=count)) +
+  geom_point() +
+  geom_smooth(se = F)
+
+bike_train |> 
+  ggpairs()
+
+plot1 <- bike_train |>
+  ggplot(aes(x=weather)) +
+  geom_bar()
