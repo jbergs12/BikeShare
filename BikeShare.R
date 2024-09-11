0e4a0ba10fa27c7c7005b9d00453a75915c0755b
Josh Bergstrom <jbergs12@byu.edu>
2024-09-11 12:49:12 -0600

    finished fitting lm model

--- BikeShare.R
+++ BikeShare.R
@@ -9,11 +9,24 @@ library(GGally)
 # Read Data
 
 bike_train <- vroom("train.csv")
-bike_train$season <- as.factor(bike_train$season)
-bike_train$weather <- as.factor(bike_train$weather)
-bike_train$workingday <- as.factor(bike_train$workingday)
-bike_train$holiday <- as.factor(bike_train$holiday)
-#bike_train$datetime <- as.factor(bike_train$datetime)
+bike_train <- bike_train  |> 
+  mutate(
+    season = as.factor(season),
+    weather = as.factor(weather),
+    workingday = as.factor(workingday),
+    holiday = as.factor(holiday)
+  )  |> 
+  select(-casual, -registered)
+
+bike_test <- vroom("test.csv")
+bike_test <- bike_test %>%
+  mutate(
+    season = as.factor(season),
+    weather = as.factor(weather),
+    workingday = as.factor(workingday),
+    holiday = as.factor(holiday)
+  )
+
 View(bike_train)
 
 # EDA
@@ -67,4 +80,18 @@ plot4 <- bike_train |>
 bike_lm <- linear_reg() |> 
   set_engine("lm") |> 
   set_mode("regression") |> 
-  fit(formula=log(count)~ . -casual -registered, data=bike_train)
+  fit(formula=log(count)~ ., data=bike_train)
+
+bike_predictions <- predict(bike_lm, new_data = bike_test)
+
+# Write Data Set to Computer
+
+kaggle_submission <- bike_predictions |> 
+  bind_cols(bike_test) |> 
+  select(datetime, .pred) |> 
+  rename(count=.pred) |> 
+  mutate(count=pmax(0, count)) |> 
+  mutate(datetime=as.character(format(datetime)),
+         count=exp(count))
+
+vroom_write(x=kaggle_submission, file="./LinearPreds.csv", delim = ",")
