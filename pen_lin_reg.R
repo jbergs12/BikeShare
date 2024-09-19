122b9e659429a358d00e3879015c61d6fc1dc6fc
Josh Bergstrom <jbergs12@byu.edu>
2024-09-18 20:00:52 -0600

    tried different penalty values

--- pen_lin_reg.R
+++ pen_lin_reg.R
@@ -19,20 +19,32 @@ bike_recipe <- recipe(count~., data = bike_train) |>
     season=factor(season,
                   levels = 1:4,
                   labels = c("Spring", "Summer", "Fall", "Winter")),
-    weather=as.factor(ifelse(weather==4, weather==3, weather)),
+    weather=as.factor(ifelse(weather==4, 3, weather)),
     holiday=as.factor(holiday),
     workingday=as.factor(workingday)) |> 
-  step_date(datetime, features = "month") |>
+  step_date(datetime, features = c("month", "dow")) |>
   step_time(datetime, features = "hour", keep_original_cols = F) |>
   step_mutate(datetime_hour=as.factor(datetime_hour),
-              datetime_month=as.factor(datetime_month)) |>
-  step_dummy(all_nominal_predictors()) |> 
+              datetime_month=as.factor(datetime_month),
+              datetime_dow=as.factor(datetime_dow)) |>
+  step_dummy(all_nominal_predictors()) |>
   step_normalize(all_predictors())
 
-# prepped_recipe = prep(bike_recipe)
-# bake(prepped_recipe, new_data=bike_test)
+# Testing different penalty combinations
 
 bike_lm <- linear_reg(penalty=2, mixture=.2) |> 
+  set_engine("glmnet")
+
+bike_lm <- linear_reg(penalty=5, mixture=.01) |> 
+  set_engine("glmnet")
+
+bike_lm <- linear_reg(penalty=.5, mixture=.7) |> 
+  set_engine("glmnet")
+
+bike_lm <- linear_reg(penalty=10, mixture=.5) |> 
+  set_engine("glmnet")
+
+bike_lm <- linear_reg(penalty=3, mixture=.05) |> 
   set_engine("glmnet")
 
 bike_workflow <- workflow() |> 
@@ -41,7 +53,6 @@ bike_workflow <- workflow() |>
   fit(data=bike_train)
 
 lin_preds <- predict(bike_workflow, new_data=bike_test)
-
 
 # Write Data Set to Computer
 
