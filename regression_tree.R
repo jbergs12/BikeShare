817c9a448aed7c1b8ed6d859a997e1d7a860644d
Josh Bergstrom <jbergs12@byu.edu>
2024-09-23 10:17:00 -0600

    Regression Tree

--- regression_tree.R
+++ regression_tree.R
@@ -6,6 +6,7 @@ library(skimr)
 library(DataExplorer)
 library(GGally)
 library(glmnet)
+library(rpart)
 
 bike_train <- vroom("train.csv")
 bike_train <- bike_train  |> 
@@ -30,23 +31,24 @@ bike_recipe <- recipe(count~., data = bike_train) |>
   step_dummy(all_nominal_predictors()) |>
   step_normalize(all_predictors())
 
-# Hypertuning and Cross Validation
+bike_tree <- decision_tree(tree_depth = tune(),
+                           cost_complexity = tune(),
+                           min_n = tune()) |> 
+  set_engine("rpart") |> 
+  set_mode("regression")
 
-bike_preg <- linear_reg(penalty=tune(),
-                        mixture=tune()) |> 
-  set_engine("glmnet")
-
-bike_preg_wf <- workflow() |> 
+bike_tree_wf <- workflow() |> 
   add_recipe(bike_recipe) |> 
-  add_model(bike_preg)
+  add_model(bike_tree)
 
-grid_of_tuning_params <- grid_regular(penalty(),
-                                      mixture(),
+grid_of_tuning_params <- grid_regular(tree_depth(),
+                                      cost_complexity(),
+                                      min_n(),
                                       levels = 5)
 
-folds <- vfold_cv(bike_train, v=10)
+folds <- vfold_cv(bike_train, v=5)
 
-CV_results <- bike_preg_wf |> 
+CV_results <- bike_tree_wf |> 
   tune_grid(resamples=folds,
             grid=grid_of_tuning_params,
             metrics=metric_set(rmse))
@@ -59,16 +61,16 @@ collect_metrics(CV_results) |>
 bestTune <- CV_results |> 
   select_best(metric="rmse")
 
-final_wf <- bike_preg_wf |> 
+final_wf <- bike_tree_wf |> 
   finalize_workflow(bestTune) |> 
   fit(data=bike_train)
 
-lin_preds <- final_wf |>
+tree_preds <- final_wf |>
   predict(new_data = bike_test)
 
 # Write Data Set to Computer
 
-kaggle_submission <- lin_preds |> 
+kaggle_submission <- tree_preds |> 
   bind_cols(bike_test) |> 
   select(datetime, .pred) |> 
   rename(count=.pred) |> 
@@ -76,28 +78,4 @@ kaggle_submission <- lin_preds |>
   mutate(datetime=as.character(format(datetime)),
          count=exp(count))
 
-vroom_write(x=kaggle_submission, file="./Tune_Pen_Preds.csv", delim = ",")
-
-# Testing different penalty combinations by hand
-
-bike_lm <- linear_reg(penalty=2, mixture=.2) |> 
-  set_engine("glmnet")
-
-bike_lm <- linear_reg(penalty=5, mixture=.01) |> 
-  set_engine("glmnet")
-
-bike_lm <- linear_reg(penalty=.5, mixture=.7) |> 
-  set_engine("glmnet")
-
-bike_lm <- linear_reg(penalty=10, mixture=.5) |> 
-  set_engine("glmnet")
-
-bike_lm <- linear_reg(penalty=3, mixture=.05) |> 
-  set_engine("glmnet")
-
-bike_workflow <- workflow() |> 
-  add_recipe(bike_recipe) |> 
-  add_model(bike_lm) |> 
-  fit(data=bike_train)
-
-lin_preds <- predict(bike_workflow, new_data=bike_test)
+vroom_write(x=kaggle_submission, file="./Tree_Preds.csv", delim = ",")
